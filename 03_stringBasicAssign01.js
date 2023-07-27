5d802b0177b1b9b2034eaa1ee50e3813bf155981
PravinBankar101 <pravinbankar246@gmail.com>
2023-07-27 21:03:27 +0530

    Practiced String programs
    Practiced String programs

---
+++ 03_stringBasicAssign01.js
@@ -0,0 +1,33 @@
+
+
+//step 1:
+console.log(`----------------Step 1:----------------`);
+
+function stringBasics(){
+    var myDreamCompany= "Amdocs";
+console.log(`My dream company is: ${myDreamCompany}`);
+}
+stringBasics();
+
+//Step 2:
+//step 2.1:
+console.log(``);
+console.log(`----------------Step 2:----------------`);
+
+var myHobby1= "Reading" ;
+var myHobby2= "Traveling" ;
+var myHobby3= "Swimming" ;
+console.log(`My Hobbies are: ${myHobby1}, ${myHobby2}, ${myHobby3}`);
+ 
+
+
+//step 2.1:
+console.log(``);
+var length_myhobby1= myHobby1.length;
+var length_myhobby2= myHobby2.length;
+var length_myhobby3= myHobby3.length;
+var totalLength_Hobbies=length_myhobby1+length_myhobby2+length_myhobby3;
+console.log(`Number of characters in Hobby1: ${length_myhobby1} `);
+console.log(`Number of characters in Hobby2: ${length_myhobby2} `);
+console.log(`Number of characters in Hobby3: ${length_myhobby3} `);
+console.log(`Total number of characters in hobbies are: ${totalLength_Hobbies}`);
