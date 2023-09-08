2a30b3a527c0e74209af1f5a8eab4c6c8c2dd25a
PravinBankar101 <pravinbankar246@gmail.com>
2023-09-08 23:45:10 +0530

    Updated 25_filterMethodAssignB.js for different conditions
    Updated 25_filterMethodAssignB.js file for different conditions

--- 25_filterMethodAssignB.js
+++ 25_filterMethodAssignB.js
@@ -17,7 +17,7 @@ const emp_monika = new Employee(77, "Monika", "IT", 40000, "Wipro");
 const emp_viny = new Employee(88, "Vinayak", "IT", 75000, "TCS");
 const emp_mahi = new Employee(99, "Mahesh", "HR", 85000, "Infy");
 
-console.log(`========================= Step 1 =========================`);
+console.log(`========================= Step 1 : Employees working in TCS =========================`);
 
 let arrayEmployees = [emp_anil, emp_radha, emp_rishi, emp_sonali, emp_monika, emp_viny, emp_mahi];
 
@@ -30,3 +30,47 @@ const employeeTcs = arrayEmployees.filter((employee)=> {
   for (const employee of employeeTcs) {
     console.log(`Company Name: ${employee.emp_company}, Employee Name: ${employee.emp_name}`);
   }
+
+console.log(` `);
+console.log(`========================= Step 2 : Average salary of employees from Wipro =========================`);
+
+const employeeWipro = arrayEmployees.filter((employee) => {
+  return employee.emp_company === "Wipro";
+});
+//console.log(employeeWipro);
+
+for (const employee of employeeWipro) {
+  console.log(`Company Name: ${employee.emp_company}, Employee Name: ${employee.emp_name}, Employee Salary: ${employee.emp_salary}`);
+}
+
+let SumSalaryWipro = 0;
+for (const employee of employeeWipro) {
+  SumSalaryWipro = SumSalaryWipro + employee.emp_salary;
+}
+
+let averageSalaryWipro = SumSalaryWipro / employeeWipro.length;
+console.log(`The average salary of employees from Wipro is: ${averageSalaryWipro}`);
+
+
+console.log(` `);
+console.log(`========================= Step 3 : Average salary of employees from Wipro or Infy  =========================`);
+
+
+const employees_WiproOrInfy = arrayEmployees.filter((employee) => {
+    return employee.emp_company === "Wipro" || employee.emp_company === "Infy"  ;
+  });
+  //console.log(employees_WiproOrInfy);
+  
+  for (const employee of employees_WiproOrInfy) {
+    console.log(`Company Name: ${employee.emp_company}, Employee Name: ${employee.emp_name}, Employee Salary: ${employee.emp_salary}`);
+  }
+  
+  let SumSalary_WiproOrInfy = 0;
+  for (const employee of employees_WiproOrInfy) {
+    SumSalary_WiproOrInfy = SumSalary_WiproOrInfy + employee.emp_salary;
+  }
+  
+  let averageSalary_WiproOrInfy = SumSalary_WiproOrInfy / employees_WiproOrInfy.length;
+  
+  console.log(`The average salary of employees from Wipro or Infy is: ${averageSalary_WiproOrInfy}`);
+
